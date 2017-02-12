6624fe60dbd4b4f420fdc9159e79690d603ce92c
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2017-02-12 16:43:11 +0800

    basically done

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+
+app.get('/:timeStr', (req,res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  let timeStr = req.params.timeStr;
+  let date = moment(timeStr);
+  if(!date.isValid()) {
+    date = moment.unix(timeStr);
+  }
+  const monthsName = [ "January", "February", "March", "April", "May", "June",
+               "July", "August", "September", "October", "November", "December" ];
+  let natural = monthsName[date.months()] + ' ' + date.get('dates') + ', ' + date.get('years');
+  let result = {
+    unix: date.unix(),
+    natural
+  };
+  result = JSON.stringify(result);
+  res.end(result);
+});
+
+app.listen(3000);
